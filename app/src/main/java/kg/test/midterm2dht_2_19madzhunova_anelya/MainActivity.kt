7f0cf64954d46f98dcccfadb6c382dfe85f6eb3e
madzhunova <madzhunova_a@auca.kg>
2021-12-02 14:27:23 +0600

    11111

---
+++ app/src/main/java/kg/test/midterm2dht_2_19madzhunova_anelya/MainActivity.kt
@@ -0,0 +1,11 @@
+package kg.test.midterm2dht_2_19madzhunova_anelya
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/kg/test/midterm2dht_2_19madzhunova_anelya/MainActivity2.kt
@@ -0,0 +1,20 @@
+package kg.test.midterm2dht_2_19madzhunova_anelya
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val Button = findViewById<Button>(R.id.button)
+
+        Button.setOnClickListener{
+            startActivity(Intent(this,MainActivity2::class.java)
+        }
+    }
+}
