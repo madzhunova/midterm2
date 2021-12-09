a49950d51a2183ad5ee7f4154b4bc8f58e65a687
madzhunova <madzhunova_a@auca.kg>
2021-12-09 14:11:36 +0600

    dfkjkdfkg

--- app/src/main/java/kg/test/midterm2dht_2_19madzhunova_anelya/MainActivity2.kt
+++ app/src/main/java/kg/test/midterm2dht_2_19madzhunova_anelya/MainActivity2.kt
@@ -5,11 +5,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import kg.test.midterm2dht_2_19madzhunova_anelya.databinding.ActivityMainBinding
+
 
 class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
         val Button = findViewById<Button>(R.id.button)
 
